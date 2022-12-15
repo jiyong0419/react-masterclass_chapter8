267da9aad637cc64c94dab4243f51160e1564241
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-12-15 21:14:04 +0900

    #8.4
    Motion의 Props ( Variants )

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,63 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
+const Circle = styled(motion.div)`
+  background-color: whitesmoke;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
   end: {
+    opacity: 1,
     scale: 1,
-    rotateZ: 360,
-    transition: { type: "spring", delay: 0.5, stiffness: 100 },
+    transition: {
+      type: "spring",
+      duration: 5,
+      bounce: 0.5,
+      delayChildren: 2,
+      staggerChildren: 0.5,
+    },
   },
 };
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
-      <motion.div></motion.div>
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
