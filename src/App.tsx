e2b801c7f3a5654741a084630c04a8dab4472b42
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-12-16 15:19:19 +0900

    #8.7
    useMotionValue( )

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* overflow: hidden; */
+  button {
+    margin: 20px 0;
+  }
 `;
 
 const Box = styled(motion.div)`
@@ -29,26 +22,29 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.3);
 `;
 
-const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  tap: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(46,204,113)", transition: { duration: 0.5 } },
-};
-
 function App() {
-  const biggerBox = useRef(null);
+  const xValue = useMotionValue(0);
+  // xValue는 모션컴포넌트의 수치를 추적한다
+  useEffect(() => {
+    xValue.onChange(() => {
+      console.log(xValue.get());
+    });
+  }, [xValue]);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBox}>
-        <Box
-          drag // drag="x" ,drag="y"
-          dragSnapToOrigin
-          dragElastic={0.5}
-          dragConstraints={biggerBox}
-        />
-      </BiggerBox>
+      <div>
+        <button
+          onClick={() => {
+            xValue.set(200);
+          }}
+        >
+          move Box 200px
+        </button>
+      </div>
+      <Box style={{ x: xValue }} drag />
     </Wrapper>
   );
 }
+//  Box 의 x값은 xValue에게 추적당한다
 
 export default App;
