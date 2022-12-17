5ee6cd8b3aca5fa82ce68b520738563c06fa1f49
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-12-18 00:57:52 +0900

    #8.11
    <AnimatePresence>

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,60 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform, useScroll } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
-  button {
-    margin: 20px 0;
-  }
 `;
 
 const Box = styled(motion.div)`
-  width: 100px;
-  height: 100px;
+  width: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.3);
+  position: absolute;
+  top: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Svg = styled(motion.svg)`
-  width: 200px;
-  height: 200px;
-  color: rgba(135, 206, 235, 1);
-  path {
-    stroke: black;
-    stroke-width: 5;
-  }
-`;
-
-const svg = {
-  start: { pathLength: 0, fill: "rgba(135, 206, 235, 0)" },
-  end: {
-    fill: "rgba(135, 206, 235, 1)",
-    pathLength: 1,
-    transition: { default: { duration: 2 }, fill: { delay: 2, duration: 1 } },
+const boxVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  exit: {
+    opacity: 0,
+    scale: 0,
+    y: 50,
+    transition: { duration: 0.2 },
   },
 };
-
 function App() {
+  const [showing, setShowing] = useState(false);
+  const toggleShowing = () => setShowing((prev) => !prev);
   return (
     <Wrapper>
-      <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-        <motion.path
-          variants={svg}
-          initial="start"
-          animate="end"
-          d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
-        />
-      </Svg>
+      <button onClick={toggleShowing}>Click</button>
+      <AnimatePresence>
+        {showing ? (
+          <Box
+            variants={boxVariants}
+            initial="initial"
+            animate="visible"
+            exit="exit"
+          />
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
-
-//  Box 의 x값은 xValue에게 추적당한다
 
 export default App;
